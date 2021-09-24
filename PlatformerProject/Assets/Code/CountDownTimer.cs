8e8af96c1825c10fa2ef08ae58bfe4b10d0f8109
LukaHrg <71827332+LukaHrg@users.noreply.github.com>
2021-09-24 17:51:22 +0100

    Physics Platformer
    A simple platformer game in Unity based around physics.

---
+++ PlatformerProject/Assets/Code/CountDownTimer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownTimer : MonoBehaviour
+{
+    public GameObject startPos;
+
+    public int countDownTime = 7;
+    public Text countDownDisplay;
+
+    private void Start()
+    {
+        StartCoroutine(CountdownStart());
+    }
+
+    IEnumerator CountdownStart()
+    {
+        while (countDownTime >= -1)
+        {
+            countDownDisplay.text = countDownTime.ToString();
+
+            yield return new WaitForSeconds(1f);
+
+            countDownTime--;
+            if(countDownTime <= 0)
+            {
+                GameObject.Find("Player").GetComponent<Health>().health--;
+                GameObject.Find("Player").GetComponent<Transform>().position = startPos.transform.position;
+                countDownTime = 30;
+            }
+        }
+
+        
+    }
+
+}
