b94cbd1332809c6438fccc2f73b50421073b8850
LukaHrg <71827332+LukaHrg@users.noreply.github.com>
2021-09-24 16:56:19 +0100

    Simple RPG Unity game
    A very simple RPG clicker game

---
+++ Simple RPG/Assets/code/HealthBarController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+
+    public Slider slider;
+
+    public Player player;
+
+    public Text health_text;
+
+    public void FixedUpdate()
+    {
+        health_text.text = "HP: " + player.currentHealth.ToString() + "/" + player.Health.ToString();
+    }
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+
+}
---
+++ Simple RPG/Assets/code/battleText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class battleText : MonoBehaviour
+{
+
+    public Text output;
+
+    public void battleOutput(string input)
+    {
+
+        if (input != null)
+        {
+            string currentText = output.text.ToString();
+            string newText = currentText + "\n" + input;
+            output.text = newText;
+        }
+        else
+        {
+            output.text = null;
+        }
+
+
+
+    }
+
+}
