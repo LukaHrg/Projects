e5df2a20f59eafa9f22a8023cabb1a4d8c242d9d
LukaHrg <71827332+LukaHrg@users.noreply.github.com>
2021-09-24 17:50:05 +0100

    Simple RPG
    A simple RPG clicker game made in Unity.

---
+++ Simple RPG/Assets/code/ExpBarController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ExpBarController : MonoBehaviour
+{
+
+    public Slider slider;
+   public int level = 1;
+
+    public Player player;
+    public HealthBarController health;
+
+    public Text expLvl_text;
+
+    private void Update()
+    {
+        
+    }
+
+    public void SetMaxExp(int Maxexp)
+    {
+        slider.maxValue = Maxexp;
+    }
+
+    public void SetExp(int exp)
+    {
+        slider.value = exp;
+    }
+}
+
